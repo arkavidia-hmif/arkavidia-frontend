d3891412f1575518daa1d5839e1ef5021f7d561a
jrandiny <jrandiny@gmail.com>
2020-10-23 23:10:59 +0700

    Tweak index page

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,14 +4,16 @@ import Hero from '../components/Hero'
 
 const IndexPage: React.FC = () => (
   <Layout title="Home | Next.js + TypeScript Example">
-    <h1>Hello Next.js 👋</h1>
-    <p>API url: {process.env.API_BASE_URL}</p>
-    <Hero />
-    <p>
-      <Link href="/about">
-        <a>About {process.env.NODE_ENV}</a>
-      </Link>
-    </p>
+    <div className="container">
+      <br />
+      <Hero />
+      <p>
+        <Link href="/about">
+          <a>About {process.env.NODE_ENV}</a>
+        </Link>
+        <p>API url: {process.env.API_BASE_URL}</p>
+      </p>
+    </div>
   </Layout>
 )
 
