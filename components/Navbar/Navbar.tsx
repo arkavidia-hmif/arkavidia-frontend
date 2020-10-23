1e972b5c747db68ba2acc5bc7ff7d9ed3b5dd29e
jrandiny <jrandiny@gmail.com>
2020-10-23 11:51:41 +0700

    Add navbar shadow on scroll

--- components/Navbar/Navbar.tsx
+++ components/Navbar/Navbar.tsx
@@ -1,5 +1,5 @@
 import Link from 'next/link'
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import NavDesktop from './NavDesktop'
 import Burger from './Burger'
 import BurgerMenu from './BurgerMenu'
@@ -12,8 +12,24 @@ const Navbar = () => {
         setOpen: setOpen
     }
 
+    const [onTop, setOnTop] = useState(true);
+
+    const handleScroll = () => {
+        console.log(window.pageYOffset)
+        if (onTop != (window.pageYOffset == 0)) {
+            setOnTop(window.pageYOffset == 0);
+        }
+    }
+
+    useEffect(() => {
+        window.addEventListener('scroll', handleScroll);
+        return () => {
+            window.removeEventListener('scroll', handleScroll)
+        }
+    });
+
     return (
-        <header>
+        <header className={onTop ? '' : 'shadow'}>
             <nav className="container-fluid max-content">
                 <div className="logo">
                     <Link href="/">
@@ -36,7 +52,13 @@ const Navbar = () => {
                     top: 0;
                     z-index: 20;
                     background: #FFFF;
+
+                    transition: box-shadow .3s cubic-bezier(.4,0,.2,1);
                 }
+                header.shadow{
+                    box-shadow: 0 2px 4px -1px rgba(0,0,0,.2), 0 4px 5px 0 rgba(0,0,0,.14), 0 1px 10px 0 rgba(0,0,0,.12);
+                }
+
                 nav {
                     display: flex;
                     width: 100%;
