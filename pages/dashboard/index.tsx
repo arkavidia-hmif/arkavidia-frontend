7b52a9b9e645644558f5267a3ca10f600c834263
arsa dg <arsaadg891@gmail.com>
2020-10-25 19:13:23 +0700

    minimal update (dashboard menu)

---
+++ pages/dashboard/index.tsx
@@ -0,0 +1,20 @@
+import Link from 'next/link'
+import Layout from '../../components/Layout'
+import DashboardWrapper from '../../components/Dashboard/DashboardWrapper'
+
+const IndexPage: React.FC = () => (
+  <Layout title="Home | Next.js + TypeScript Example">
+    <div className="container">
+      <DashboardWrapper />
+      <br />
+      <p>
+        <Link href="/about">
+          <a>About {process.env.NODE_ENV}</a>
+        </Link>
+        <p>API url: {process.env.API_BASE_URL}</p>
+      </p>
+    </div>
+  </Layout>
+)
+
+export default IndexPage
