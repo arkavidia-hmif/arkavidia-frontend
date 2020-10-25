9fae5adda655d2994a866e315f835744f109df53
jrandiny <jrandiny@gmail.com>
2020-10-25 18:21:37 +0700

    Add size props for ColorfulHeader

--- components/ColorfulHeader.tsx
+++ components/ColorfulHeader.tsx
@@ -5,17 +5,22 @@ type Props = {
   children: React.ReactNode,
   headingLevel?: 1 | 2 | 3 | 4 | 5 | 6;
   color?: string;
+  size?: string;
 }
 
 type HeadingTag = 'h1' | 'h2' | 'h3' | 'h4' | 'h5' | 'h6';
 
-const ColorfulHeader: React.FC<Props> = ({ children, headingLevel = 1, color = Theme.headerColors.plbl }) => {
+const ColorfulHeader: React.FC<Props> = ({ children, headingLevel = 1, color = Theme.headerColors.plbl, size }) => {
   const Tag = `h${headingLevel}` as HeadingTag;
 
   return (
     <>
       <Tag><span>{children}</span></Tag>
       <style jsx>{`
+        ${Tag} {
+          margin: 0;
+        }
+
         span{
           background-size: 100%;
           background-image: ${color};
@@ -26,6 +31,8 @@ const ColorfulHeader: React.FC<Props> = ({ children, headingLevel = 1, color = T
           text-fill-color: transparent;
           -webkit-text-fill-color: transparent; 
           -moz-text-fill-color: transparent;
+
+          ${size ? 'font-size:' + size + ';' : ''}
         }
       `}</style>
     </>
