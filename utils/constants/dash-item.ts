c31eb422daa20274ee242656a53277b35cb9eb66
arsa dg <arsaadg891@gmail.com>
2020-10-25 19:13:23 +0700

    minimal update (dashboard menu)

---
+++ utils/constants/dash-item.ts
@@ -0,0 +1,24 @@
+const DashItems = [
+    {
+      title: 'ANNOUNCEMENT',
+      path: '/',
+    },
+    {
+      title: 'PROFILE',
+      path: '/',
+    },
+    {
+      title: 'COMPETITIONS',
+      path: '/',
+    },
+    {
+      title: 'PRE-EVENTS',
+      path: '/',
+    },
+    {
+      title: 'EVENTS',
+      path: '/',
+    },
+  ]
+  
+  export default DashItems;
