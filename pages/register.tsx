66f7a7c5222970fc03ac0dda39e43e26bb1bf49a
Akromi Afif <socakromiafif@gmail.com>
2020-10-27 01:02:54 +0700

    benerin register

---
+++ pages/register.tsx
@@ -0,0 +1,10 @@
+import Layout from '../components/Layout'
+import Register from '../components/Register'
+
+const RegisterPage: React.FC = () => (
+  <Layout title="Register">
+    <Register />
+  </Layout>
+)
+
+export default RegisterPage
