a29034dd91ad3a1452b96f9391a3e1c0ab41a9f1
Akromi Afif <socakromiafif@gmail.com>
2020-10-26 14:54:53 +0700

    add register page

--- components/Login.tsx
+++ components/Login.tsx
@@ -1,101 +1,90 @@
 import React from 'react'
 
-function Register() {
+function Login() {
     return (
-        <div className="flex-container">
+        <div className="main">
             <div className="left">
-                <h1>Registrasi Akun</h1>
-                <hr />
+                <h1>Login ke Dashboard</h1>
+                <hr></hr>
                 <form>
-                    <label>Nama Lengkap</label>
-                    <input type="text" placeholder="John Doe" />
                     <label>Alamat Email</label>
                     <input type="text" placeholder="johndoe@gmail.com" />
                     <label>Kata Sandi</label>
-                    <input type="password" placeholder="*********" />
-                    <label>Ulang Kata Sandi</label>
-                    <input type="password" />
-                    <button type="submit">Daftar</button>
-                    <a href="#">Sudah punya akun ?</a>
+                    <input type="password" placeholder="********" />
+                    <button type="submit">Login</button>
+                    <p>Lupa kata sandi ? <span>Reset</span></p>
+                    <p>Belum terdaftar ? <span>Register</span></p>
                 </form>
+
             </div>
-            <div className="right">
-                <img src="/img/bg-white.png" />
-            </div>
+            <img src="/img/bg.png"/>
             <style jsx>
                 {`
-                    * {
-                        box-sizing: border-box;
+                    .left {
+                        display: inline-block;
+                        // border: 1px solid red;
+                        width: 45%;
+                        height: auto;
+                        margin-left: 70px;
+                        float: left;
+                    }
+
+                    img {
+                        position: absolute;
+                        width: 55%;
+                        // border: 1px solid blue;
+                        float: right;
+                        z-index: -1;
                     }
                     
-                    .flex-container {
-                        display: flex;
-                        flex-direction: row;
-                        height: auto;
-                        background: linear-gradient(180deg, rgba(255, 255, 255, 0.26) 20.31%, rgba(255, 163, 186, 0.38) 53.65%, rgba(168, 142, 215, 0.56) 100%);
-                    }
-
-                    .left {
-                        flex: 45%;
-                        padding-left: 50px;
-                    }
-                      
-                    .right {
-                        padding: 10px;
-                        flex: 55%;
-                    }
-
-                    @media (max-width: 800px) {
-                        .flex-container {
-                          flex-direction: column;
-                        }
-                    }
-
                     h1 {
                         font-family: Viga;
                         font-style: normal;
                         font-weight: normal;
                         font-size: 60px;
-                        line-height: 81px;
                         letter-spacing: 0.01em;
-                        display: block;
+                        width: 554px;
+                        /* standard gradient background */
                         background: linear-gradient(rgba(254, 120, 154, 1), rgba(98, 63, 162, 1));
+                      
+                        /* clip hackery */
                         -webkit-background-clip: text;
                         -webkit-text-fill-color: transparent;
                     }
 
                     hr {
-                        width: 30%;
+                        width: 35%;
+                        float: left;
+                        display: block;
+                        color: red;
                         height: 10px;
                         background: linear-gradient(90deg, #FE789A 0%, #623FA2 100%);
-                        float: left;
-                        margin-top: -50px;
+                        left: 0;
+                        margin-top: -40px;
                     }
 
                     form {
-                        height: auto;
-                        width: 78%;
+                        margin-top: 40px;
+                        display: block;
                     }
 
                     label {
                         font-family: Roboto;
                         font-style: normal;
                         font-weight: bold;
-                        font-size: 22px;
-                        line-height: 26px;
+                        font-size: 24px;
                         display: block;
-                        color: #000000;
                         margin-top: 10px;
                     }
 
                     input {
-                        width: 100%;
+                        margin-top: 10px;
+                        width: 60%;
                         border: none;
                         padding: 5px 0 10px 0;
                         border-bottom: 2px solid black;
                         box-sizing: border-box;
                         background: none;
-                        margin-top: 10px;
                     }
 
                     input[type="text"], input[type="password"] {
@@ -117,46 +106,50 @@ function Register() {
                     }
 
                     button {
-                        display: inline-block;
+                        display: block;
                         background: #FE789A;
                         border-radius: 10px;
-                        width: 110px;
-                        height: 45px;
-                        border: none;
                         color: white;
-                        font-family: Roboto;
-                        font-style: normal;
-                        font-size: 20px;
-                        margin-right: 140px;
-                        cursor: pointer;
-                        margin-top: 20px;
-                    }
-
-                    // button:hover {
-                    //     border: 2px solid #FE789A;
-                    //     background: none;
-                    //     color: #7446A1;
-                    // }
-
-                    a {
-                        display: inline-block;
+                        border: none;
+                        width: 120px;
+                        height: 40px;
                         font-family: Roboto;
                         font-style: normal;
                         font-weight: bold;
-                        font-size: 20px;
-                        color: #7446A1;
-                        text-decoration: none;
+                        font-size: 22px;
+                        cursor: pointer;
+                        margin: 30px 0 30px 0;
                     }
 
-                    img {
-                        width: 660px;
-                        float: right;
-                        margin: -45px 0 0 0;
+                    p {
+                        font-family: Roboto;
+                        font-style: normal;
+                        font-weight: normal;
+                        font-size: 22px;
+                        line-height: 26px;
+                        font-weight: bold;
+                        color: #7446A1;
+                        line-height: 0.3;
                     }
+
+                    span {
+                        color: #FE789A;
+                    }
+
+                    .main {
+                        overflow: hidden;
+                        margin: 0;
+                        padding: 0;
+                        position: relative;
+                        width: 100%;
+                        height: 100vh;
+                        background: linear-gradient(180deg, rgba(255, 255, 255, 0.26) 20.31%, rgba(255, 163, 186, 0.38) 53.65%, rgba(168, 142, 215, 0.56) 100%);
+                    }
+                    
                 `}
             </style>
         </div>
     )
 }
 
-export default Register
+export default Login
