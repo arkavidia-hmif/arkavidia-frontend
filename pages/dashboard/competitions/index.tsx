f051dad00ebaa08e679e4b9f453345f15316b2dd
arsa dg <arsaadg891@gmail.com>
2020-10-30 15:05:16 +0700

    add CompetitionsCard Component

---
+++ pages/dashboard/competitions/index.tsx
@@ -0,0 +1,12 @@
+import CompetitionsCard from '../../../components/Dashboard/Competitions/CompetitionsCard'
+import DashboardWrapper from '../../../components/Dashboard/DashboardWrapper'
+import Layout from '../../../components/Layout'
+
+const IndexPage: React.FC = () => (
+  <Layout title="Announcement | Arkavidia 7.0" background="white">
+    <DashboardWrapper />
+    <CompetitionsCard />
+  </Layout>
+)
+
+export default IndexPage
