97c4395f204bf69abdc570c38fda6c7a94828350
jrandiny <jrandiny@gmail.com>
2020-10-31 21:48:07 +0700

    Add arrow for mobile submenu

--- components/navbar/BurgerMenu.tsx
+++ components/navbar/BurgerMenu.tsx
@@ -25,9 +25,12 @@ const BurgerMenu: React.FC<Props> = ({ open }) => {
             const toggle = toggleState[index][0];
             const setToggle = toggleState[index][1];
 
+            const arrowClass = "arrow " + (toggle ? "up" : "down");
+            console.log(arrowClass);
+
             return (
               <>
-                <a onClick={() => setToggle(!toggle)}>{link.text}</a>
+                <a onClick={() => setToggle(!toggle)}>{link.text} <i className={arrowClass}></i></a>
                 <BurgerSubMenu items={link.submenu} toggle={toggle} />
               </>
             );
@@ -43,41 +46,55 @@ const BurgerMenu: React.FC<Props> = ({ open }) => {
       </div>
 
       <style jsx>{`
+        i.arrow {
+          border: solid white;
+          border-width: 0 4px 4px 0;
+          display: inline-block;
+          padding: 4px ;
+
+          transition: transform 0.2s;
+        }
+
+        .arrow.up {
+          transform: rotate(-135deg);
+        }
+
+        .arrow.down {
+          transform: rotate(45deg);
+        }
+        
+        .burger-menu {
+          display: none;
+          flex-direction: column;
+          justify-content: center;
+          background:  #FE789A;
+          height: auto;
+          width: 100%;
+          text-align: center;
+          padding: 1rem 0;
+          position: absolute;
+          top: ${Dimen.navbarMobileHeight};
+          transition: transform 0.3s ease-in-out;
+          z-index: -10;
+        }
+
+        a {
+          font-family: Viga;
+          font-size: 1rem;
+          text-transform: uppercase;
+          padding: 1rem 0;
+          font-weight: bold;
+          letter-spacing: 0.5rem;
+          color: white;
+          text-decoration: none;
+          transition: color 0.3s linear;
+        }
+
+        @media (max-width: 1300px) {
           .burger-menu {
-            display: none;
-            flex-direction: column;
-            justify-content: center;
-            background:  #FE789A;
-            height: auto;
-            width: 100%;
-            text-align: center;
-            padding: 1rem;
-            position: absolute;
-            top: ${Dimen.navbarMobileHeight};
-            transition: transform 0.3s ease-in-out;
-            z-index: -10;
+            display: flex;
           }
-
-          a {
-            font-family: Viga;
-            font-size: 1rem;
-            text-transform: uppercase;
-            padding: 1rem 0;
-            font-weight: bold;
-            letter-spacing: 0.5rem;
-            color: white;
-            text-decoration: none;
-            transition: color 0.3s linear;
-          }
-
-          a:hover {
-            color: pink;
-          }
-          @media (max-width: 1300px) {
-            .burger-menu {
-              display: flex;
-            }
-          }
+        }
       `}</style>
       <style jsx>{`
           .burger-menu {
