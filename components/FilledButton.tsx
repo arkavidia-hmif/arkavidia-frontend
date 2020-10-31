cd5bdf439b9b833023138b559234bced74b9a80a
jrandiny <jrandiny@gmail.com>
2020-11-01 00:14:34 +0700

    Add optional loading to FilledButton component

--- components/FilledButton.tsx
+++ components/FilledButton.tsx
@@ -5,16 +5,18 @@ type Props = {
   onClick?: () => void,
   text: string,
   padding?: string,
-  color?: ButtonColor
+  color?: ButtonColor,
+  loading?: boolean
 }
 
-const FilledButton: React.FC<Props> = ({ onClick, text, padding, color = Theme.buttonColors.pinkButton }) => (
+const FilledButton: React.FC<Props> = ({ onClick, text, padding, loading, color = Theme.buttonColors.pinkButton }) => (
   <>
-    <div onClick={onClick} style={{ padding }}>
+    <div id="container" onClick={onClick} style={{ padding }}>
+      <div id="loader"></div>
       <b>{text}</b>
     </div>
     <style jsx>{`
-      div {
+      #container {
         display: inline-block;
         padding: 0.5rem 1rem;
         border-radius: 10px;
@@ -25,9 +27,48 @@ const FilledButton: React.FC<Props> = ({ onClick, text, padding, color = Theme.b
         cursor: pointer;
 
         transition: background-color 0.1s;
+
+        position: relative;
       }
-      div:hover {
+
+      #container:hover {
         background-color: ${color.hover};
+      }
+
+      #loader {
+        display: none;
+
+        border: 4px solid white;
+        border-top: 4px solid transparent;
+        border-radius: 50%;
+        width: 30px;
+        height: 30px;
+        animation: spin 2s linear infinite;
+
+        position: absolute;
+        margin: auto;
+        left: 0;
+        right: 0;
+        top: 0;
+        bottom: 0;
+      }
+      
+      @keyframes spin {
+        0% { transform: rotate(0deg); }
+        100% { transform: rotate(360deg); }
+      }
+    `}</style>
+    <style jsx>{`
+      #loader {
+        ${loading ? 'display: block' : ''}
+      }
+
+      #container {
+        ${loading ? 'color: ' + color.main : ''}
+      }
+
+      #container:hover {
+        ${loading ? 'color: ' + color.hover : ''}
       }
     `}</style>
   </>
