3cb5506db324356bd50c06601c11242aa27d90b7
jrandiny <jrandiny@gmail.com>
2020-10-31 13:08:39 +0700

    Implement saving state and endpoint protection

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,20 +3,48 @@ import Head from 'next/head';
 import 'bootstrap/dist/css/bootstrap-reboot.min.css';
 import 'bootstrap/dist/css/bootstrap-grid.min.css';
 import { AuthContext, AuthContextType } from '../utils/context/auth';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import { AuthData } from '../interfaces/auth';
 import { SWRConfig } from 'swr';
+import { useRouter } from 'next/dist/client/router';
 
 const MyApp: React.FC<AppProps> = ({ Component, pageProps }) => {
   const [authenticated, setAuthenticated] = useState(false);
   const [auth, setAuth] = useState<AuthData>();
 
+  const authenticatedKey = process.env['LOCAL_STORAGE_AUTHENTICATED'] || 'authenticated_dev';
+  const authKey = process.env['LOCAL_STORAGE_AUTH'] || 'auth_dev';
+
+  useEffect(() => {
+    setAuthenticated(localStorage.getItem(authenticatedKey) === 'true');
+    const savedAuth = localStorage.getItem(authKey);
+    if (savedAuth) {
+      setAuth(JSON.parse(savedAuth));
+    }
+  }, []);
+
+  const setAndSaveAuthenticated = (newValue: boolean) => {
+    setAuthenticated(newValue);
+    localStorage.setItem(authenticatedKey, newValue ? 'true' : 'false');
+  };
+
+  const setAndSaveAuth = (newValue: AuthData) => {
+    setAuth(newValue);
+    localStorage.setItem(authKey, JSON.stringify(newValue));
+  };
+
   const authContext: AuthContextType = {
     authenticated,
     auth,
-    setAuthenticated,
-    setAuth
+    setAuthenticated: setAndSaveAuthenticated,
+    setAuth: setAndSaveAuth
   };
+
+  const router = useRouter();
+
+  if (router.pathname.startsWith('/event') && !authenticated) {
+    router.push(`/login?continue=${router.pathname}`);
+  }
 
   const swrConfig = {};
 
