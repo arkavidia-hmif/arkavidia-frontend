df6433601df72d6990dcd115b1c8a6bd9a48e8d5
jrandiny <jrandiny@gmail.com>
2020-10-31 14:54:47 +0700

    Implement expiry check for api request

--- components/provider/ApiProvider.tsx
+++ components/provider/ApiProvider.tsx
@@ -20,11 +20,21 @@ const ApiProvider: React.FC<Props> = ({ children }) => {
     xsrfHeaderName: 'X-CSRFToken'
   });
 
-  if (authContext.authenticated && authContext.auth) {
-    apiClient.defaults.headers.common['Authorization'] = `Bearer ${authContext.auth.token}`;
-  } else {
-    apiClient.defaults.headers.common['Authorization'] = null;
-  }
+  apiClient.interceptors.request.use((config) => {
+    const currentEpoch = new Date().getTime() / 1000;
+    console.log(currentEpoch);
+
+    if (authContext.authenticated && authContext.auth) {
+      if (authContext.auth.exp > currentEpoch) {
+        config.headers.common['Authorization'] = `Bearer ${authContext.auth.token}`;
+      } else {
+        authContext.setAuthenticated(false);
+        authContext.setAuth();
+      }
+    }
+
+    return config;
+  });
 
   const contextValue: ApiContextType = {
     axios: apiClient
