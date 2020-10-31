bdd6366298d1aa9aec62a47ec5099d3a3461bbfe
arsa dg <arsaadg891@gmail.com>
2020-10-31 17:33:26 +0700

    rebase master + fixlint

--- pages/dashboard/events/index.tsx
+++ pages/dashboard/events/index.tsx
@@ -1,12 +1,12 @@
-import DashboardWrapper from '../../../components/page/Dashboard/DashboardWrapper'
-import EventsCard from '../../../components/page/Dashboard/Events/EventsCard'
-import Layout from '../../../components/Layout'
+import DashboardWrapper from '../../../components/page/Dashboard/DashboardWrapper';
+import EventsCard from '../../../components/page/Dashboard/Events/EventsCard';
+import Layout from '../../../components/Layout';
 
 const IndexPage: React.FC = () => (
   <Layout title="Events | Arkavidia 7.0" background="white">
     <DashboardWrapper />
     <EventsCard />
   </Layout>
-)
+);
 
-export default IndexPage
+export default IndexPage;
