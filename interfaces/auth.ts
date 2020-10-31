68d16ed5501b204d1d38da209ff8aae7909c0e02
jrandiny <jrandiny@gmail.com>
2020-10-31 14:41:12 +0700

    Update auth data interface

--- interfaces/auth.ts
+++ interfaces/auth.ts
@@ -1,8 +1,14 @@
 export type AuthData = {
   token: string,
+  exp: number,
   user: {
     fullName: string,
+    dateJoined: string,
     email: string,
-    institution: string
+    currentEducation: string | null,
+    phoneNumber: string | null,
+    institution: string | null
+    birthDate: string | null,
+    address: string | null
   }
 }
