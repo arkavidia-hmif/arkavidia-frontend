5f41c749949f6e992d36cd8293f8b84593c024db
Jones Napoleon Autumn <13518086@std.stei.itb.ac.id>
2020-11-01 03:14:35 +0700

    done for submissionprogress component

--- components/page/Dashboard/Competitions/SubmissionProgress.tsx
+++ components/page/Dashboard/Competitions/SubmissionProgress.tsx
@@ -1,8 +1,8 @@
 import * as React from "react";
-import Link from "next/link";
 
 type Props = {
   setActive: (integer: number) => void;
+  active?: number;
 };
 // type ClickableProps = {
 //   icon: string;
@@ -12,68 +12,82 @@ type Props = {
 //   return <div>defr</div>;
 // };
 
-const SubmissionProgress: React.FC<Props> = ({ setActive }) => {
+const timData = [
+  {
+    text: "Informasi Tim",
+    link: "../../../img/dashboard/submission/tim.png"
+  },
+  {
+    text: "Anggota Tim",
+    link: "../../../img/dashboard/submission/anggota.png"
+  }
+];
+
+const anggotaData = [
+  {
+    text: "Foto Diri",
+    link: "../../../img/dashboard/submission/lingkaran.png"
+  },
+  {
+    text: "KTP/KTM",
+    link: "../../../img/dashboard/submission/jampasir.png"
+  },
+  {
+    text: "SKMA",
+    link: "../../../img/dashboard/submission/check.png"
+  },
+  {
+    text: "Bukti Pembayaran",
+    link: "../../../img/dashboard/submission/lingkaran.png"
+  },  
+];
+
+const SubmissionProgress: React.FC<Props> = ({ active, setActive }) => {
+  const handleClick = (num: number) => {
+    setActive(num);
+    console.log('efrg');
+  };
   return (
     <div className="container mb-3 card">
-      <h2 onClick={() => setActive(1)}>Nama Tim </h2>
-      <p className="mt-1">Nama Lomba</p>
-
+      <h2>Nama Tim </h2>
+      <p className="">Nama Lomba</p>
       <div className="not-dropdown">
         <div className="title">TIM</div>
       </div>
       <div className="dropdown">
         <ul className="list">
-          {/* routenya diganti yaa */}
-          <Link href="/dashboard/competitions/ctf"><a>
-            <li className="mt-1">
-              <img src="../../../img/dashboard/submission/tim.png" className="mr-3"/>
-              Informasi Tim
-            </li>  
-          </a></Link>
-          <Link href="/dashboard/competitions/ctf/anggota-tim"><a>
-            <li className="mt-1">
-              <img src="../../../img/dashboard/submission/anggota.png" className="mr-3"/>
-              Anggota Tim
-            </li>
-          </a></Link>
+          {
+            timData.map((datum, i) => 
+              <li onClick={() => handleClick(i)} key={datum.text} className={active === i ? "active" : ""}>
+                {active === i && <span id='right-roller'></span>}
+                <img src={datum.link} className="mr-3"/>
+                {datum.text}
+              </li>
+            )
+          }
+          
         </ul>
       </div>
       <div className="not-dropdown">
-        <div className="title">PRASYARAT PENDAFTARAN </div>
+        <div className="title">PRASYARAT PENDAFTARAN</div>
       </div>
       <div className="dropdown">
         <ul className="list">
-          {/* routenya diganti yaa */}
-          <Link href="/dashboard/competitions/ctf"><a>
-            <li className="mt-1">
-              <img src="../../../img/dashboard/submission/lingkaran.png" className="mr-3"/>
-              Foto Diri
-            </li>  
-          </a></Link>
-          <Link href="/dashboard/competitions/ctf"><a>
-            <li className="mt-1">
-              <img src="../../../img/dashboard/submission/jampasir.png" className="mr-3"/>
-              KTP/KTM
-            </li>
-          </a></Link>
-          <Link href="/dashboard/competitions/ctf"><a>
-            <li className="mt-1">
-              <img src="../../../img/dashboard/submission/check.png" className="mr-3"/>
-              SKMA
-            </li>  
-          </a></Link>
-          <Link href="/dashboard/competitions/ctf"><a>
-            <li className="mt-1">
-              <img src="../../../img/dashboard/submission/lingkaran.png" className="mr-3"/>
-              Bukti Pembayaran
-            </li>
-          </a></Link>
+          {
+            anggotaData.map((datum, i) => 
+              <li onClick={() => handleClick(i + 2)} key={datum.text} className={active === (i + 2) ? "active" : ""}>
+                {active === i + 2 && <span id='right-roller'></span>}
+                <img src={datum.link} className="mr-3"/>
+                {datum.text}
+              </li>
+            )
+          }          
         </ul>
       </div>
       <style jsx>{`
         h2 {
           margin-bottom: 0;
-          margin-top: 2rem;
+          margin-top: 1rem;
           font-family: Viga;
           font-size: 23.4375px;
           line-height: 31px;
@@ -87,12 +101,30 @@ const SubmissionProgress: React.FC<Props> = ({ setActive }) => {
         ul{
           list-style:none;
         }
+
         .dropdown li{
-          color: #161F24;
-          max-width: 25rem;
-          max-height: 3rem;
+          color: #161F24;          
+          min-height: 2.4rem;          
+          line-height: 2.4rem;
+          cursor: pointer;
           padding: 0.5rem;
+          position: relative;
           font-size: 0.875rem;
+        }
+        .dropdown li.active {
+          background: rgba(251, 188, 200, .3);
+          margin: 0 -10px 0 -15px;
+          padding: 0.5rem calc(0.5rem + 15px);
+        }
+        #right-roller {
+          position: absolute;
+          background: #FE94AB;
+          opacity: .5;
+          border-radius: 10.3125px;
+          width: 10px;
+          height: 3.4rem;
+          top: 0;
+          right: -5px;
         }
         .dropdown{
           padding: 0;
@@ -101,22 +133,15 @@ const SubmissionProgress: React.FC<Props> = ({ setActive }) => {
           padding: 0;
         }
         p {
-          font-size: 11.7187px;
-          line-height: 14px;
+          font-size: .8rem;          
         }
         .title {
           color: rgba(0, 0, 0, 0.6);
-          font-size: 16.4062px;
-          line-height: 19px;
+          font-size: 1.2rem;
+          line-height: 1.4rem;
         }
         .card {
-          background: linear-gradient(
-              180deg,
-              #ffffff 0%,
-              rgba(255, 255, 255, 0) 100%
-            ),
-            linear-gradient(180deg, #ffbdea 0%, rgba(255, 255, 255, 0) 100%),
-            #f3a9dd;
+          background: linear-gradient(180deg, #FFFFFF 0%, rgba(255, 255, 255, 0) 100%), linear-gradient(180deg, #FFBDEA 0%, rgba(255, 255, 255, 0) 100%), #F3A9DD;
           border: 0.46875px solid #05058d;
           box-sizing: border-box;
           border-radius: 10px;
@@ -124,7 +149,7 @@ const SubmissionProgress: React.FC<Props> = ({ setActive }) => {
 
         .content {
           font-family: Roboto;
-          font-size: 1.125rem;
+          font-size: 1rem;
           color: #646464;
         }
 
