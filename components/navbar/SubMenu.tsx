7678717895cce5cdec6354b4d6c740bd2ef96e70
jrandiny <jrandiny@gmail.com>
2020-10-31 22:48:00 +0700

    Add hover effect to desktop navbar

--- components/navbar/SubMenu.tsx
+++ components/navbar/SubMenu.tsx
@@ -2,17 +2,17 @@ import * as React from 'react';
 import Link from 'next/link';
 
 type ArrayType = {
-    path: string,
-    text: string
+  path: string,
+  text: string
 }
 
 type Props = {
-    items: ArrayType[],
-    hover: boolean,
-    setHover: (input: boolean) => void,
+  items: ArrayType[],
+  hover: boolean,
+  setHover: (input: boolean) => void,
 }
 
-const SubMenu : React.FC<Props> = ({items, hover, setHover}) => {
+const SubMenu: React.FC<Props> = ({ items, hover, setHover }) => {
   return (
     <div className="sub-menu" onMouseOver={() => setHover(true)} onMouseLeave={() => setHover(false)}>
       <ul className="mr-3">
@@ -24,33 +24,41 @@ const SubMenu : React.FC<Props> = ({items, hover, setHover}) => {
       </ul>
       <style jsx>{`
         .sub-menu {
-            visibility: ${hover ? 'visible' : 'hidden'};
-            opacity: ${hover ? 1 : 0};
-            background: #FFFF;
-            width: 300px;
-            position: absolute;
-            top: 3.4rem;
-            box-shadow: 0px 8px 16px 0px rgba(0,0,0,0.2);
-            transition: visibility 0.2s, opacity 0.2s linear;
+          background: #FFFF;
+          width: 300px;
+          position: absolute;
+          top: 2.5rem;
+          box-shadow: 0px 8px 16px 0px rgba(0,0,0,0.2);
+          transition: visibility 0.2s, opacity 0.2s linear;
         }
         
         ul {
-            display: flex;
-            flex-direction: column;
-            list-style: none;
+          display: flex;
+          flex-direction: column;
+          list-style: none;
         }
 
         li {
-            padding: 1rem 0;
+          padding: 1rem 0;
         }
 
         a {
-            color: #623FA2;
-            font-family: Viga;
-            font-style: normal;
-            font-weight: normal;
-            font-size: 1.5rem;
-            text-decoration: none;
+          color: #623FA2;
+          font-family: Viga;
+          font-style: normal;
+          font-weight: normal;
+          font-size: 1.5rem;
+          text-decoration: none;
+        }
+
+        a:hover {
+          color: #987ccc;
+        }
+      `}</style>
+      <style jsx>{`
+        .sub-menu {
+          visibility: ${hover ? 'visible' : 'hidden'};
+          opacity: ${hover ? 1 : 0};
         }
       `}</style>
     </div>
