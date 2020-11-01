c9c948dd40338963e349d96a43039d2052ea813e
arsa dg <arsaadg891@gmail.com>
2020-11-01 09:26:41 +0700

    add basic popup for profile

--- components/page/Dashboard/Profile/ModalProfile.tsx
+++ components/page/Dashboard/Profile/ModalProfile.tsx
@@ -1,11 +1,26 @@
 import * as React from 'react';
 import FilledButton from '../../../FilledButton';
+import Popup from './Popup';
 
 const ModalProfile: React.FC = () => {
+  const [isOpen, setIsOpen] = React.useState(false);
+ 
+  const togglePopup = () => {
+    setIsOpen(!isOpen);
+  };
+  
   return (
     <div className="container mb-3">
       <div className="container-fluid main">
-        <FilledButton text="Edit Profile" padding="0.75rem 3rem" />
+        <FilledButton text="Edit Profile" padding="0.75rem 3rem" onClick={togglePopup}/>
+        {isOpen && <Popup
+          content={<div>
+            <form>
+              blabla
+            </form>
+          </div>}
+          handleClose={togglePopup}
+        />}
       </div>
       <style jsx>{`
        
