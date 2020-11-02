9c0b554228e0646c8d47c137bc381d6f2c111192
jrandiny <jrandiny@gmail.com>
2020-11-02 11:57:20 +0700

    Fix blinking page on dynamic redirection to protected route

--- pages/dynamic-redirector.tsx
+++ pages/dynamic-redirector.tsx
@@ -1,11 +1,18 @@
 import { useRouter } from "next/dist/client/router";
+import { useContext } from "react";
+import { AuthContext } from "../utils/context/auth";
 
 const DynamicRedirector: React.FC = () => {
   const router = useRouter();
+  const authContext = useContext(AuthContext);
 
   if (typeof window !== "undefined") {
     const currentPath = window.location.pathname;
-    router.replace(currentPath);
+    if (currentPath.startsWith('/dashboard') && !authContext.authenticated) {
+      router.replace('/login?continue=/dashboard');
+    } else {
+      router.replace(currentPath);
+    }
   }
 
   return (<></>);
