ce7f227792bf4aff85f7dbdae0b8eb7d30ef70d5
jrandiny <jrandiny@gmail.com>
2020-11-04 10:07:58 +0700

    Add gravatar support for team member

--- components/page/Dashboard/Competitions/member/MemberCard.tsx
+++ components/page/Dashboard/Competitions/member/MemberCard.tsx
@@ -1,13 +1,20 @@
+import * as MD5 from "crypto-js/md5";
 import { TeamMember } from "../../../../../interfaces/team";
+import { defaultProfileUrl } from "../../../../../utils/constants/profile-picture";
 
 type Props = {
   team: TeamMember
 }
 
 const MemberCard: React.FC<Props> = ({ team }) => {
+
+  const gravatarHash = MD5(team.email.toLowerCase()).toString();
+  const gravatarUrl = `https://www.gravatar.com/avatar/${gravatarHash}?d=${defaultProfileUrl}`;
+
+
   return (
     <div id="member-container">
-      <img src="/img/competitions/member.png" />
+      <img src={gravatarUrl} />
       <p className="m-0 ml-3">{team.fullName} {team.isTeamLeader ? '(ketua)' : ''}</p>
       <style jsx>{`
         #member-container {
@@ -15,6 +22,11 @@ const MemberCard: React.FC<Props> = ({ team }) => {
           flex-direction: row;
 
           align-items: center;
+        }
+
+        img {
+          border-radius: 50%;
+          width: 60px;
         }
       `}</style>
     </div>
