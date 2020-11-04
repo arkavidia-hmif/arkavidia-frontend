ac44bf40d5e342b0c793cd0a0cecb569ff79c40e
Jones Napoleon Autumn <13518086@std.stei.itb.ac.id>
2020-11-04 10:49:17 +0700

    lint

--- components/page/Dashboard/Competitions/member/MemberCard.tsx
+++ components/page/Dashboard/Competitions/member/MemberCard.tsx
@@ -3,19 +3,19 @@ import { TeamMember } from "../../../../../interfaces/team";
 import { defaultProfileUrl } from "../../../../../utils/constants/profile-picture";
 
 type Props = {
-  team: TeamMember
-}
+  team: TeamMember;
+};
 
 const MemberCard: React.FC<Props> = ({ team }) => {
-
   const gravatarHash = MD5(team.email.toLowerCase()).toString();
   const gravatarUrl = `https://www.gravatar.com/avatar/${gravatarHash}?d=${defaultProfileUrl}`;
-
 
   return (
     <div id="member-container">
       <img src={gravatarUrl} />
-      <p className="m-0 ml-3">{team.fullName} {team.isTeamLeader ? '(ketua)' : ''}</p>
+      <p className="m-0 ml-3">
+        {team.fullName} {team.isTeamLeader ? "(ketua)" : ""}
+      </p>
       <style jsx>{`
         #member-container {
           display: flex;
