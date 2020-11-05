aefb31c2053e0a838444d9007aff89d4f27b29db
Jones Napoleon Autumn <13518086@std.stei.itb.ac.id>
2020-11-05 17:52:04 +0700

    change language

--- utils/transformer/profile.ts
+++ utils/transformer/profile.ts
@@ -21,50 +21,49 @@ export const checkTruth = async (
   if (!profile) {
     throw new Error("Masalah koneksi");
   }
-  if (isEmpty(birthDate)) {
-    if (isValidDate(profile?.birthDate)) data.birthDate = profile?.birthDate;
-    else throw new Error("Birth date is not valid");
-  } else if (isValidDate(birthDate)) {
-    data.birthDate = birthDate;
-  } else throw new Error("Birth date is not valid");
-
   if (isEmpty(fullName)) {
     if (isValidString(profile?.fullName, 75)) data.fullName = profile?.fullName;
-    else throw new Error("Full name is not valid");
+    else throw new Error("Nama tidak valid");
   } else if (isValidString(fullName, 75)) {
     data.fullName = fullName;
-  } else throw new Error("Full name is not valid");
+  } else throw new Error("Nama tidak valid");
 
   if (isEmpty(institution)) {
     if (isValidString(profile?.institution, 75)) {
       data.institution = profile?.institution;
-    } else throw new Error("Institution name is not valid");
+    } else throw new Error("Asal sekolah/universitas tidak valid");
   } else if (isValidString(institution, 75)) {
     data.institution = institution;
-  } else throw new Error("Institution name is not valid");
+  } else throw new Error("Asal sekolah/universitas tidak valid");
 
   if (isEmpty(currentEducation)) {
     if (isValidString(profile?.currentEducation, 30)) {
       data.currentEducation = profile?.currentEducation;
-    } else throw new Error("Current education is not valid");
+    } else throw new Error("Status pendidikan tidak valid");
   } else if (isValidString(currentEducation, 30)) {
     data.currentEducation = currentEducation;
-  } else throw new Error("Current education is not valid");
+  } else throw new Error("Status pendidikan tidak valid");
 
   if (isEmpty(phoneNumber)) {
     if (isValidString(profile?.phoneNumber, 20)) {
       data.phoneNumber = profile?.phoneNumber;
-    } else throw new Error("Phone number is not valid");
+    } else throw new Error("Nomor telepon tidak valid");
   } else if (isValidString(phoneNumber, 20)) {
     data.phoneNumber = phoneNumber;
-  } else throw new Error("Phone number is not valid");
+  } else throw new Error("Nomor telepon tidak valid");
 
   if (isEmpty(address)) {
     if (isValidString(profile?.address, 75)) data.address = profile?.address;
-    else throw new Error("Address is not valid");
+    else throw new Error("Alamat tidak valid");
   } else if (isValidString(address, 75)) {
     data.address = address;
-  } else throw new Error("Address is not valid");
+  } else throw new Error("Alamat tidak valid");
+  if (isEmpty(birthDate)) {
+    if (isValidDate(profile?.birthDate)) data.birthDate = profile?.birthDate;
+    else throw new Error("Tanggal lahir tidak valid");
+  } else if (isValidDate(birthDate)) {
+    data.birthDate = birthDate;
+  } else throw new Error("Tanggal lahir tidak valid");
 
   return data;
 };
