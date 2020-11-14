7292d3acb36f6b32cafd9c996d62fef1328c6aa4
jrandiny <jrandiny@gmail.com>
2020-11-14 15:02:58 +0700

    Fix date typo

--- utils/constants/dates.ts
+++ utils/constants/dates.ts
@@ -13,23 +13,23 @@ const Dates = {
   ],
   arkavtalks: [
     {
-      date: "Sabtu,  20 Februaru 2021",
+      date: "Sabtu,  20 Februari 2021",
       time: "Advanced 13.00 - 14.30",
     },
     {
-      date: "Minggu,  21 Februaru 2021",
+      date: "Minggu,  21 Februari 2021",
       time: "Advanced 13.00 - 14.30",
     },
     {
-      date: "Senin,  22 Februaru 2021",
+      date: "Senin,  22 Februari 2021",
       time: "Public 19.00 - 20.30",
     },
     {
-      date: "Rabu,  24 Februaru 2021",
+      date: "Rabu,  24 Februari 2021",
       time: "Public 19.00 - 20.30",
     },
     {
-      date: "Jumat,  26 Februaru 2021",
+      date: "Jumat,  26 Februari 2021",
       time: "Public 19.00 - 20.30",
     }
   ]
