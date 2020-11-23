100000806266d228edbab10b05d870a8e9374e4c
jrandiny <jrandiny@gmail.com>
2020-11-23 12:40:36 +0700

    Disable arkavtalks faq

--- pages/event/arkavidia-talks.tsx
+++ pages/event/arkavidia-talks.tsx
@@ -6,15 +6,15 @@ import TalksGallery from "../../components/page/event/talks/TalksGallery";
 import Dates from "../../utils/constants/dates";
 import DateColor from "../../utils/constants/date-color";
 import DateContainer from "../../components/DateContainer";
-import ArkavTalksFAQ from "../../components/page/event/talks/ArkavTalksFAQ";
+// import ArkavTalksFAQ from "../../components/page/event/talks/ArkavTalksFAQ";
 
 const Talks: React.FC = () => (
   <Layout {...BackgroundProps.talks}>
     <div className="container">
       <EventHeader {...EventItems.talks} >
-        <DateContainer datesDetail={Dates.arkavtalks} color={DateColor.event} colorAbout={DateColor.eventAbout}/>
+        <DateContainer datesDetail={Dates.arkavtalks} color={DateColor.event} colorAbout={DateColor.eventAbout} />
       </EventHeader>
-      <ArkavTalksFAQ />
+      {/* <ArkavTalksFAQ /> */}
       <TalksGallery />
     </div>
   </Layout>
