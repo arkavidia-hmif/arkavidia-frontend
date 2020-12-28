20dccffebb1d8ac4c9f61dd667cc4fdd4dd05590
jrandiny <jrandiny@gmail.com>
2020-12-28 17:41:28 +0700

    Fix action and status for public talks

--- components/dashboard/talks/TalksTableRow.tsx
+++ components/dashboard/talks/TalksTableRow.tsx
@@ -5,7 +5,7 @@ import { Event, EventRegistration } from "interfaces/event";
 import { getEventRegistrationDetail } from "api/event";
 import { ApiContext } from "utils/context/api";
 import Spinner from "components/Spinner";
-import { TALKS_REGISTRATION_STAGE, TALKS_PAYMENT_TASK_INDEX } from "utils/constants/talks-stage";
+import { TALKS_REGISTRATION_STAGE, TALKS_PAYMENT_TASK_INDEX, TALKS_PUBLIC_CAT, TALKS_ADVANCED_CAT } from "utils/constants/talks-stage";
 
 interface Props {
   event: Event,
@@ -22,6 +22,8 @@ const TalksTableRow: React.FC<Props> = ({ event, idx, participant, popupCb }) =>
     data,
     error
   } = useSWR(() => {
+    if (event.category === TALKS_PUBLIC_CAT) return "";
+
     if (participant?.id) {
       return `/mainevent/registrants/${participant.id}/`;
     } else {
@@ -36,45 +38,71 @@ const TalksTableRow: React.FC<Props> = ({ event, idx, participant, popupCb }) =>
   const isRegistered = !!participant;
 
   const clickHandler = () => {
-    if (isRegistered) {
-      router.push(`/dashboard/arkav-talks/${participant?.id}/payment`);
-    } else {
-      popupCb(event);
+    if (event.category === TALKS_ADVANCED_CAT) {
+      if (isRegistered) {
+        router.push(`/dashboard/arkav-talks/${participant?.id}/payment`);
+      } else {
+        popupCb(event);
+      }
+    } else if (event.category === TALKS_PUBLIC_CAT) {
+      if (!isRegistered) {
+        popupCb(event);
+      }
     }
   };
 
   const getStatusColumn = () => {
-    if (isRegistered) {
-      if (!data) {
-        return <Spinner size="25px" height="19px" />;
-      }
-      if (error) {
-        return "Error";
-      }
+    if (event.category === TALKS_ADVANCED_CAT) {
+      if (isRegistered) {
+        if (!data) {
+          return <Spinner size="25px" height="19px" />;
+        }
+        if (error) {
+          return "Error";
+        }
 
-      const activeStage = data.stages.find(entry => entry.id === data.activeStageId);
+        const activeStage = data.stages.find(entry => entry.id === data.activeStageId);
 
-      if (activeStage) {
-        if (activeStage.name === TALKS_REGISTRATION_STAGE) {
-          // Registration stage
-          const taskId = activeStage.tasks[TALKS_PAYMENT_TASK_INDEX].id;
-          const response = data.taskResponses.find(entry => entry.taskId === taskId);
+        if (activeStage) {
+          if (activeStage.name === TALKS_REGISTRATION_STAGE) {
+            // Registration stage
+            const taskId = activeStage.tasks[TALKS_PAYMENT_TASK_INDEX].id;
+            const response = data.taskResponses.find(entry => entry.taskId === taskId);
 
-          if (!response) {
-            return "Belum konfirmasi pembayaran";
-          } else if (response.status === "completed") {
-            return "Terdaftar";
-          } else if (response.status === "awaiting_validation") {
-            return "Menunggu validasi pembayaran";
-          } else if (response.status === "rejected") {
-            return "Pembayaran ditolak";
+            if (!response) {
+              return "Belum konfirmasi pembayaran";
+            } else if (response.status === "completed") {
+              return "Terdaftar";
+            } else if (response.status === "awaiting_validation") {
+              return "Menunggu validasi pembayaran";
+            } else if (response.status === "rejected") {
+              return "Pembayaran ditolak";
+            }
           }
         }
+        return "Error";
+      } else {
+        return "Belum terdaftar";
       }
-      return "Error";
-    } else {
-      return "Belum terdaftar";
+    } else if (event.category === TALKS_PUBLIC_CAT) {
+      return isRegistered ? "Terdaftar" : "Belum terdaftar";
     }
+  };
+
+  const getActionLabel = () => {
+    if (!isRegistered) {
+      return "Daftar";
+    } else {
+      if (event.category === TALKS_ADVANCED_CAT) {
+        return "Pembayaran";
+      } else if (event.category === TALKS_PUBLIC_CAT) {
+        return "Sudah terdaftar";
+      }
+    }
+  };
+
+  const isActionable = () => {
+    return !(event.category === TALKS_PUBLIC_CAT && isRegistered);
   };
 
   return (
@@ -84,7 +112,7 @@ const TalksTableRow: React.FC<Props> = ({ event, idx, participant, popupCb }) =>
       <td>{event.category}</td>
       {isRegistered && <td>{getStatusColumn()}</td>}
       <td>
-        <a onClick={clickHandler}>{isRegistered ? "Pembayaran" : "Daftar"}</a>
+        <a onClick={clickHandler}>{getActionLabel()}</a>
       </td>
       <style jsx>{`
         tr {background-color: #ffffff;}
@@ -98,9 +126,13 @@ const TalksTableRow: React.FC<Props> = ({ event, idx, participant, popupCb }) =>
         }  
 
         a {
-          color: #613FB6;
+          color: ${isActionable() ? "#613FB6" : "gray"};
           font-weight: bold;
-          cursor: pointer;
+          cursor: ${isActionable() ? "pointer" : ""};
+        }
+
+        a:hover {
+          text-decoration: ${isActionable() ? "" : "none"};
         }
       `}</style>
     </tr>
