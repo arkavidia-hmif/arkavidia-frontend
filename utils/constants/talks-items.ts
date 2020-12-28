a45c06dc2f2fc0b9131af546d52fc06f8780e6bf
jrandiny <jrandiny@gmail.com>
2020-12-28 15:16:27 +0700

    Add talks item for public talks

--- utils/constants/talks-items.ts
+++ utils/constants/talks-items.ts
@@ -30,7 +30,74 @@ export const TalksItems = [
     hero: "https://picsum.photos/id/2/400/300",
     items: [
       {
-
+        session: "Day 1 - Sesi 1",
+        date: "Senin, 22 Februari 2021",
+        time: "19.00 - 21.00 WIB",
+        title: "Below The Tip of The Iceberg - Di Balik Perkembangan Sosial Media",
+        description: "Prior to joining Indosat as VP of ICT Security Solution, Wildan worked for several top consulting firms, Accenture, PwC SEA Consulting, IBM, and Bank Mandiri. He has exposure in Digital Strategy; Enterprise Architecture and IT Strategy; Cyber Risk, Trust, and Privacy; and Business Process Improvement under his belt. He led and involved in various projects for Financial Services, Telecommunication, Higher Education, Resources, Logistics, and Public Sector.",
+        speaker: "Wildan Aliviyarda",
+        credential: "Vice President of ICT Security Solution, Indosat Ooredoo",
+        profilePicture: "/img/talks/basic-1-1-speaker.png"
+      },
+      {
+        session: "Day 1 - Sesi 2",
+        date: "Senin, 22 Februari 2021",
+        time: "19.00 - 21.00 WIB",
+        title: "Citizen 4.0 - Apakah Kamu Sudah Termasuk?",
+        description: "Graduated from Universitas Indonesia in 2018, Kurniawan then joined Nokia Indonesia in 2017 as a Fullstack Developer. He then pursue his career in Software Engineering Field by joining Tokopedia as Nakama in 2018 as a Software Engineer. Now he continue his career as a Senior Software Engineer in one of the biggest Edutech Startup in Indonesia - Ruang Guru.",
+        speaker: "Kurniawan Hendi Wijaya",
+        credential: "Senior Software Engineer at RuangGuru, Ex Fullstack Developer at Nokia",
+        profilePicture: "/img/talks/basic-1-2-speaker.png"
+      },
+      {
+        session: "Day 2 - Sesi 1",
+        date: "Rabu, 24 Februari 2021",
+        time: "19.00 - 21.00 WIB",
+        title: "IT Adventures: Prepare to Build an Amazing Career in Tech",
+        description: "Tjan Marco Orlando is a Customer Success Account Manager at Microsoft in Indonesia, which are known to be one of the largest and the most revolutionary tech company in the world. He is currently on his 3rd year working on Microsoft, with his previous experience entrusted to be the Technical Account Manager for the same company. Graduated Cum Laude from ITB on 2017, he won a bunch of prestigious business plan competitions and awarded with plenty of honors such as XLFL and Accenture Future Technology Leaders during his college years",
+        speaker: "Tjan Marco Orlando",
+        credential: "Customer Success Account Manager at Microsoft Indonesia",
+        profilePicture: "/img/talks/basic-2-1-speaker.png",
+      },
+      {
+        session: "Day 2 - Sesi 2",
+        date: "Rabu, 24 Februari 2021",
+        time: "19.00 - 21.00 WIB",
+        title: "My Career Path: From Bandung to Silicon Valley",
+        description: "Fransiska is the Head of Engineering at Mamikos - #1 property tech platform in Indonesia. She was the Founder & CEO at Prelo, a retail sharing economy platform that was acquired by Bukalapak in 2018 and led her to become one of Forbes 30 under 30 Asia list-makers. Previously, Fransiska went to Institut Teknologi Bandung and stinted at Microsoft, Google Summer of Code, and The President's Delivery Unit. Fransiska is also a cofounder of AugMI labs, an award-winning biomedical startup in Silicon Valley and has been named one of ten inspirational female tech entrepreneurs in South East Asia by Forbes.",
+        speaker: "Fransiska PW",
+        credential: "Head of Engineering at Mamikos, Forbes 30 under 30",
+        profilePicture: "/img/talks/basic-2-2-speaker.png"
+      },
+      {
+        session: "Day 3 - Sesi 1",
+        date: "Jumat, 26 Februari 2021",
+        time: "19.00 - 21.00 WIB",
+        title: "What You Didn't Know About Building a Startup",
+        description: "Yosia is a Co-Founder, / CTO at Paper.id, a leading Invoicing & Payment SaaS platform in Indonesia. He was actively helping a number of medium-sized companies implementing various tech projects and involved in several startups. His experience and his studies in both Computer Science in UCLA and MBA in LMU have shaped his perspectives on what a good tech product should be like.",
+        speaker: "Yosia Sugialam",
+        credential: "Co-Founder at Paper.id",
+        profilePicture: "/img/talks/basic-3-1-speaker.png"
+      },
+      {
+        session: "Day 3 - Sesi 2",
+        date: "Jumat, 26 Februari 2021",
+        time: "19.00 - 21.00 WIB",
+        title: "The Challenging World of Creative Industry Startups",
+        description: "Adi, CEO and Co-Founder of Sagara Technology and Teknologi.id is a technopreneur himself. The startup he lead, Teknologi.id, has educated more than a hundred thousands people, both in Indonesia and worldwide. Graduated from Telkom University in 2013, he has proven himself as a technopreneur by joining Founder Institute in Silicon Valley in 2017 and now he is currently pursuing his degree for Executive Studies in world-famous Harvard Business School",
+        speaker: "Adi Arriansyah",
+        credential: "Founder and CEO, Sagara Technology",
+        profilePicture: "/img/talks/basic-3-2-speaker.png"
+      },
+      {
+        session: "Day 3 - Sesi 3",
+        date: "Jumat, 26 Februari 2021",
+        time: "19.00 - 21.00 WIB",
+        title: "The True Key in Game Changing IT Companies",
+        description: "Ari is an experienced Client Technical Leader with a demonstrated history of working in the computer software industry, including IBM and Microsoft. With more than 20 years of experience in the leading tech company, she is a professional in business alliances, enterprise software, business development, partner Management, data analytic and enterprise architectures. Ari is an engineering professional with a Doctorate focused in Islamic Economics Studies, and currently also a lecturer in Universitas Paramadina",
+        speaker: "Ari Pratiwi",
+        credential: "Cloud and Cognitive Technical Leader, IBM Indonesia",
+        profilePicture: "/img/talks/basic-3-3-speaker.png"
       }
     ] as Array<TalksCarouselItem>
   }
