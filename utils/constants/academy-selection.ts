8547cb6fa4d73f609e00fb51fd878c6284a71476
jrandiny <jrandiny@gmail.com>
2021-01-23 11:51:40 +0700

    Update academy registration start date

--- utils/constants/academy-selection.ts
+++ utils/constants/academy-selection.ts
@@ -5,7 +5,7 @@ export const AcademySelection: Array<AcademyEntry> = [
     title: "AI Academy",
     company: "IBM",
     date: "Sabtu, 6 Februari 2021",
-    registrationDate: "22 Januari - 5 Februari 2021",
+    registrationDate: "23 Januari - 5 Februari 2021",
     logo: "/img/academy/ibm.png",
     guidebookLink: "https://link.arkavidia.id/guidebook_academy_ai"
   },
@@ -13,7 +13,7 @@ export const AcademySelection: Array<AcademyEntry> = [
     title: "Start-Up Academy",
     company: "Eduka",
     date: "Sabtu, 13 Februari 2021",
-    registrationDate: "22 Januari - 6 Februari 2021",
+    registrationDate: "23 Januari - 6 Februari 2021",
     registrationFee: "Rp 20.000,-",
     logo: "/img/academy/eduka.png",
     guidebookLink: "https://link.arkavidia.id/guidebook_academy_startup"
@@ -23,7 +23,7 @@ export const AcademySelection: Array<AcademyEntry> = [
     subTitle: "Intro to Shader in Unity 3D",
     company: "Agate",
     date: "Sabtu, 30 Februari 2021",
-    registrationDate: "22 Januari - 13 Februari 2021",
+    registrationDate: "23 Januari - 13 Februari 2021",
     registrationFee: "Rp 20.000,-",
     logo: "/img/academy/agate.png",
     guidebookLink: "https://link.arkavidia.id/guidebook_academy_game"
