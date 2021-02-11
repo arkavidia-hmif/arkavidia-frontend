61c8d1119aef1af33de23dd6fd43f15bdebb2129
jrandiny <jrandiny@gmail.com>
2021-02-11 11:30:05 +0700

    Update sponsor logo

--- utils/constants/competition-items.ts
+++ utils/constants/competition-items.ts
@@ -4,7 +4,7 @@ export default {
     imageAlt: "logo arkalogica",
     title: "Arkalogica",
     sponsor: "Jooalan",
-    sponsorLogo: "https://arkavidia.nyc3.digitaloceanspaces.com/logo-jooalan.png",
+    sponsorLogo: "https://arkavidia.nyc3.cdn.digitaloceanspaces.com/logo-jooalan.png",
     paragraph:
       "Arkalogica adalah kompetisi yang mengasah kemampuan peserta dalam penggunaan logika dan menyelesaikan masalah secara logis. Jenis-jenis permasalahan yang diberikan antara lain berbagai soal logika dan game menarik.",
     bg: "-webkit-linear-gradient(left,#835FBF,#00FFFF);",
@@ -13,7 +13,7 @@ export default {
     image: "/img/competitions/ctf-logo.png",
     imageAlt: "logo ctf",
     sponsor: "CrediBook",
-    sponsorLogo: "https://arkavidia.nyc3.digitaloceanspaces.com/logo-credibook.png",
+    sponsorLogo: "https://arkavidia.nyc3.cdn.digitaloceanspaces.com/logo-credibook.png",
     title: "Capture the Flag",
     paragraph:
       "Capture the Flag adalah cabang lomba di bidang keamanan siber. Pada lomba ini, setiap peserta saling bersaing sebagai tim untuk mengumpulkan poin sebanyak mungkin dengan memecahkan persoalan yang diberikan. Dalam kurun waktu yang terbatas, setiap tim harus menggunakan kakas-kakas dan pengetahuan yang mereka miliki untuk mencari, mengakali, dan mengeksploitasi celah-celah keamanan untuk mendapatkan poin dalam bentuk flag tersembunyi dalam setiap soal.",
@@ -24,7 +24,7 @@ export default {
     imageAlt: "logo cp",
     title: "Competitive Programming",
     sponsor: "CrediBook",
-    sponsorLogo: "https://arkavidia.nyc3.digitaloceanspaces.com/logo-credibook.png",
+    sponsorLogo: "https://arkavidia.nyc3.cdn.digitaloceanspaces.com/logo-credibook.png",
     paragraph:
       "Kompetisi Competitive Programming (CP) menguji kemampuan analisis pemecahan masalah (problem solving) dan berpikir komputasional dengan cara menyelesaikan persoalan yang diberikan dengan bahasa pemrograman tertentu dalam batasan waktu dan memori yang telah ditentukan.",
     bg: "-webkit-linear-gradient(left,#835FBF,#00FFFF);",
@@ -34,7 +34,7 @@ export default {
     imageAlt: "logo gamedev",
     title: "Arkav Game Jam",
     sponsor: "Jooalan",
-    sponsorLogo: "https://arkavidia.nyc3.digitaloceanspaces.com/logo-jooalan.png",
+    sponsorLogo: "https://arkavidia.nyc3.cdn.digitaloceanspaces.com/logo-jooalan.png",
     paragraph:
       "Arkav Game Jam adalah kompetisi pembuatan aplikasi permainan yang merupakan bagian dari acara Arkavidia 7.0. Dalam perlombaan ini, setiap tim peserta yang beranggotakan maksimal 5 orang membuat sebuah game dalam waktu 48 jam. Dengan tema yang baru akan diketahui saat lomba akan dimulai, tantangan terbesar bagi para peserta adalah bagaimana menemukan ide dan mengimplementasikan ide tersebut dari dasar dalam waktu singkat.",
     bg: "-webkit-linear-gradient(left,#835FBF,#00FFFF);",
@@ -44,7 +44,7 @@ export default {
     imageAlt: "logo datavidia",
     title: "Datavidia",
     sponsor: "CrediBook",
-    sponsorLogo: "https://arkavidia.nyc3.digitaloceanspaces.com/logo-credibook.png",
+    sponsorLogo: "https://arkavidia.nyc3.cdn.digitaloceanspaces.com/logo-credibook.png",
     paragraph:
       "Data science merupakan bidang yang sedang mendapatkan sorotan belakangan ini karena kecanggihan dan keluasan aplikasinya. Data Arkavidia merupakan kompetisi untuk menilai kepiawaian peserta serta bertujuan untuk meningkatkan kompetensi peserta di bidang ini.",
     bg: "-webkit-linear-gradient(left,#835FBF,#00FFFF);",
